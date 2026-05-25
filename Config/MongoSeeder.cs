fa6dfcd84f5d4d1a298aad4fa46de913a4d0c77f
30011287 <30011287@MD-T103-611590>
2026-05-25 12:52:47 +0800

    Add MongoSeeder, and API versioning support
    Fixed a typo in the MongoDB connection string property (`ConnectingString` -> `ConnectionString`). Registered repositories and the `MongoSeeder` service in the DI container. Added API versioning with a default version of 1.0.
    
    Introduced the `MongoSeeder` class to seed initial data into the MongoDB database, including default users. Updated `MongoSeeder.cs` with necessary `using` directives for MongoDB and models.

---
+++ Config/MongoSeeder.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using TripWise.Api.Models;
+
+namespace TripWise.Api.Config
+{
+    public class MongoSeeder
+    {
+        private readonly IMongoCollection<User> _userCollection;
+
+        public MongoSeeder(IMongoCollection<User> usersCollection)
+        {
+            _userCollection = usersCollection;
+        }
+
+        public async Task SeedAsync()
+        {
+
+            // check if data already exists
+            var existingUsers = await _userCollection.CountDocumentsAsync(_ => true);
+            if (existingUsers == 0)
+            {
+
+                var users = new List<User>
+            {
+                new User { Name = "Admin", Email = "admin@example.com" },
+                new User { Name = "Test User", Email = "test@example.com" }
+            };
+                await _userCollection.InsertManyAsync(users);
+            }
+        }
+    }
+}
